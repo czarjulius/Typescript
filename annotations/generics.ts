a63e7fc7772ef463edab9220cdea4f18a8cb4663
czarjulius <juliusczar.jc@gmail.com>
2021-02-11 11:56:13 +0100

    generics

--- annotations/generics.ts
+++ annotations/generics.ts
@@ -1,17 +1,79 @@
-class HoldNumber{
-   data: number;
-}
-const newNumber = new HoldNumber()
+class ArrayOfNumbers{
+   constructor(public collection: number[]){}
+ 
+   get(index: number): number{
+     return this.collection[index]
+   }
+ }
+ 
+ class ArrayOfStrings{
+   constructor(public collection: string[]){}
+ 
+   get(index: number): string{
+     return this.collection[index]
+   }
+ }
 
-class HoldString{
-   data: string;
-}
+ class ArrayOfAnything<T>{
+    constructor(public collection: T[]){}
 
-const newString = new HoldString()
+    get(index: number): T{
+       return this.collection[index]
+    }
+ }
 
+ const arr = new ArrayOfAnything<string>(['a','d','t','o'])
 
-class HoldAnything<T>{
-   data: T;
-}
+ // Generics with functions
 
-const newHoldAnything = new HoldAnything<number>()
+ function printStrings(arr: string[]): void{
+    for (const string of arr) {
+       console.log(string);
+       
+    }
+ }
+
+ function printNumbers(arr: number[]): void{
+    for (const num of arr) {
+       console.log(num);
+       
+    }
+ }
+
+ function printAnything<T>(arr: T[]): void{
+    for (const anything of arr) {
+       console.log(anything);
+       
+    }
+ }
+
+ printAnything<number>([1,2,3,4,5,6])
+
+ //Generic Constraints
+
+ class Car{
+    print(){
+       console.log('I am a car');
+       
+    }
+ }
+ class House{
+    print(){
+       console.log('I am a house');
+       
+    }
+ }
+
+ interface Printable{
+    print(): void;
+ }
+
+ function printHousesOrCars<T extends Printable>(arr: T[]):void{
+    for (const item of arr) {
+       item.print()
+    }
+
+ } 
+
+ printHousesOrCars<House>([new House(), new House()])
+ printHousesOrCars<Car>([new Car(), new Car()])
--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,20 +1,9 @@
 import {MatchReader} from './MatchReader'
-import { CsvFileReader } from './CsvFileReader'
-import { ConsoleReport } from './reportTargets/ConsoleReport'
-import { WinsAnalysis } from './analyzers/WinsAnalysis'
 import { Summary  } from './Summary'
-import { HtmlReport  } from './reportTargets/HtmlReport'
-
-const csvFileReader = new CsvFileReader('football.csv');
-
-const matchReader = new MatchReader(csvFileReader);
-matchReader.load();
-
-// const summary = new Summary(
-//   new WinsAnalysis('Man United'),
-//   new HtmlReport()
-// )
-
+ 
+const matchReader = MatchReader.fromCsv('football.csv');
 const summary = Summary.winsAnalysisWithHtmlReport('Man United')
 
+matchReader.load();
 summary.buildAndPrintReport(matchReader.matches)
+
