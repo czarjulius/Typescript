e8c6bb0f421e8ad4d3ee375669dd54f7dd511dce
czarjulius <juliusczar.jc@gmail.com>
2021-02-16 13:21:53 +0100

    render user form with random age

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,9 +1,7 @@
+import { UserForm } from './views/UserForm';
 import { User } from './models/User';
 
-const user = User.buildUser({id: 1})
+const user = User.buildUser({name: "Name", age: 30})
+const userForm = new UserForm(document.getElementById('root'), user)
 
-user.on('change', ()=>{
-  console.log(user);
-  
-})
-user.fetch()
+userForm.render();
--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -2,6 +2,7 @@ import {Model} from './Model'
 import {ApiSync} from './ApiSync'
 import {Eventing} from './Eventing'
 import {Attributes} from './Attributes'
+import { Collection } from './Collection';
 
 const rootUrl = 'http://localhost:3000/users';
 
@@ -18,5 +19,14 @@ export class User extends Model<UserProps> {
       new Eventing(),
       new ApiSync<UserProps>(rootUrl)
     );
+  }
+
+  static buildUserCollection(): Collection<User, UserProps> {
+    return new Collection<User, UserProps>(rootUrl, (json: UserProps) => User.buildUser(json))
+  }
+
+  setRandomAge():void{
+    const age = Math.round(Math.random() * 100);
+    this.set({age})
   }
 }
